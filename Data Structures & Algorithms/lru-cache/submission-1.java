901729c1282f011d6e6a27921e7b0864fb394474
sagarsocrates <45390237+sagarsocrates@users.noreply.github.com>
2026-07-29 22:02:50 +0530

    Add: lru-cache - submission-1

---
+++ Data Structures & Algorithms/lru-cache/submission-1.java
@@ -0,0 +1,76 @@
+private class Node {
+    int key;
+    int val;
+    Node next;
+    Node prev;
+
+    Node(int key, int val){
+        this.key = key;
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class LRUCache {
+
+    int capacity = 0;
+    HashMap<Integer,Node> cache;
+    Node head;
+    Node tail = new Node(-1,-1);
+    
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.cache = new HashMap<>();
+        head = new Node(-1,-1);
+        tail = new Node(-1,-1);
+        head.next = tail;
+        tail.prev = head;
+    }
+    
+    public int get(int key) {
+        if(!cache.containsKey(key)){
+            return -1;
+        }
+        Node hit = cache.get(key);
+        remove(hit);
+        insert(hit);
+        return hit.val;
+    }
+    
+    public void put(int key, int value) {
+        if(cache.containsKey(key)){
+            Node existing = cache.get(key);
+            existing.val = value;
+            remove(existing);
+            insert(existing);
+        }
+        else{
+            if(cache.size() == capacity){
+                Node lru = tail.prev;
+                remove(lru);
+                cache.remove(lru.key);
+            }   
+
+            Node node = new Node(key,value);
+            insert(node);
+            cache.put(key,node);
+        }
+        
+    }
+
+    public void insert(Node node){
+        Node temp = head.next;
+        head.next = node;
+        node.next = temp;
+        node.prev = head;
+        temp.prev = node;
+    }
+
+    public void remove(Node node){
+        Node prev = node.prev;
+        Node next = node.next;
+        prev.next = next;
+        next.prev = prev;
+    }
+}
